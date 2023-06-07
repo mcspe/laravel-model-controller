2010eb87ced39414f467d8e4af92b6c844e10419
mcspe <mcspezzaferro@gmail.com>
2023-06-07 15:51:39 +0200

    [add] header with links

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,15 @@
-<header>header</header>
+<header>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" data-bs-theme="dark">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link {{Route::currentRouteName() === 'home' ? 'active' : ''}}" href="{{ route('home') }}">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link {{Route::currentRouteName() === 'contacts' ? 'active' : ''}}" href="{{ route('contacts') }}">Contacts</a>
+          </li>
+      </div>
+    </div>
+  </nav>
+</header>
