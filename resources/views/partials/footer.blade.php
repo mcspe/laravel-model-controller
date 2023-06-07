04521b362082ab3dc7f95e02a7f47b03dbbc71c0
mcspe <mcspezzaferro@gmail.com>
2023-06-07 15:59:53 +0200

    [add] provsional footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,6 @@
-<footer>footer</footer>
+<footer>
+  <div class="container-fluid text-white text-center p-3 bg-dark fixed-bottom">
+    <span>Credits </span>
+    <a href="https://github.com/mcspe">@mcspe</a>
+  </div>
+</footer>
